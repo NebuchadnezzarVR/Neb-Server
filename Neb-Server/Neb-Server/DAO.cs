ef2ec9774beeb224c510911e0fe3a3bdea1ce4ed
Tom Lazar <thomas.lazar@marquette.edu>
2017-01-21 03:08:41 -0600

    merged

---
+++ Neb-Server/Neb-Server/DAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Neb_Server
+{
+	class Dao
+	{
+		private static readonly Lazy<MongoClient> LazyClient = new Lazy<MongoClient>(() => new MongoClient(@"mongodb://localhost"));
+
+		public static MongoClient Instance => LazyClient.Value;
+	}
+}
